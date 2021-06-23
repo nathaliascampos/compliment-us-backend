477348b74c3d5f7aa451a9fa8d214db8ec79f4f8
Nathália Campos <campossnathalia@gmail.com>
2021-06-23 09:17:04 -0300

    feat: create database, entities, migrations, repositories, services and controllers

--- src/server.ts
+++ src/server.ts
@@ -1,18 +1,27 @@
+import "reflect-metadata";
 import express from 'express';
+
+import { router } from "./routes";
+
+import "./database";
 
 const app = express();
 
+// para usar o json no body é preciso inserir essa linha
+app.use(express.json());
 
-// @GET 
-app.get("/test", (request, response) => {
-    // Request => entrando 
-    // Responde => saindo 
-    return response.send('Olá NLW!')
-})
+// // @GET
+// app.get("/test", (request, response) => {
+//     // Request => entrando 
+//     // Responde => saindo 
+//     return response.send('Olá NLW!')
+// })
 
-app.post("/test-post", (request, response) => {
-    return response.send('Olá NLW!')
-})
+// app.post("/test-post", (request, response) => {
+//     return response.send('Olá NLW!')
+// })
+
+app.use(router);
 
 // http://localhost:3000 
 app.listen(3000, () => console.log("Server is running."))
