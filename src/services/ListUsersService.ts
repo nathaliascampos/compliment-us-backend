5a62f3b6e0110274f14b9c580ec83b3db417b706
Nathália Campos <campossnathalia@gmail.com>
2021-06-27 20:21:13 -0300

    feat: exclude password when list users

--- src/services/ListUsersService.ts
+++ src/services/ListUsersService.ts
@@ -1,13 +1,13 @@
 import { getCustomRepository } from "typeorm"
 import { UsersRepositories } from "../repositories/UsersRepositories"
-
+import { classToPlain } from "class-transformer"
 class ListUsersService {
     async execute() {
         const usersRepositories = getCustomRepository(UsersRepositories)
 
         const user = await usersRepositories.find()
 
-        return user;
+        return classToPlain(user);
     }
 
 }
